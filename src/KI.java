4bef9c38e2c5fcc726d0fc169adeee9337df3581
Merten Nagel <m.nagel@fz-juelich.de>
2014-10-30 15:39:20 +0100

    KI hinzugefuegt

---
+++ src/KI.java
@@ -0,0 +1,13 @@
+ /**
+  * @date: 30.10.14
+  * @author: merten
+  */
+public class KI{
+  
+  
+  
+  public static void main(String[] args) {
+    System.out.println("Klappt ein Pull auch?");
+    
+  }
+}
