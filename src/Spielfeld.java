fdd058da67a22320fa740e58d7a39158e854ccf1
Ann-Cathrin <a.weger@fz-juelich.de>
2014-11-18 19:56:52 +0100

    Test ob Push-Funktioniert

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -33,8 +33,8 @@ public class Spielfeld extends JFrame {
         // Bilder setzen
         try {
         	// Die Pfade koennten abweichen :/
-			grass = new ImageIcon("grass.jpg");
-			water = new ImageIcon("water.jpg");
+			grass = new ImageIcon("/src/Bilder/grass.jpg");
+			water = new ImageIcon("../Bilder/bombe.jpg"); //Test da kein water.jpg vorhanden
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
