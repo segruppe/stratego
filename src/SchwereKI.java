cd72f815248ed4ad1b490e4c80badc3f358eecf2
a.weger <a.weger@fz-juelich.de>
2014-12-02 16:02:22 +0100

    schwere KI

--- src/SchwereKI.java
+++ src/SchwereKI.java
@@ -19,28 +19,21 @@ public class SchwereKI extends KI {
 
     @Override
     public void setzeStartAufstellung() {
+        boolean tarnung;
         // zuerst Fahne setzen:
         int r=(int)(Math.random()*4);
         switch (r){
             case 0: this.figurenSatzKI.get(0).setPosition(new Position(0,0)); // linke Ecke
-                    int b=(int)(Math.random()*3);
-                    switch(b){
-                        case 0: this.figurenSatzKI.get(listTmp.size()-1).setPosition(new Position(0,1));
-                                this.figurenSatzKI.get(listTmp.size()-2).setPosition(new Position(1,0));
-                    }
+                    break;
+
             case 1: this.figurenSatzKI.get(0).setPosition(new Position(0, 9));break; // rechte Ecke
             case 2: this.figurenSatzKI.get(0).setPosition(new Position(0, 4));break; // Mitte oberste Reihe
-            case 3: this.figurenSatzKI.get(0).setPosition(new Position((int)(Math.random()*10), (int)(Math.random()*2)));
+            case 3: this.figurenSatzKI.get(0).setPosition(new Position((int)(Math.random()*2), (int)(Math.random()*10)));
         }
-
-        int index=2;
         spielfeld.panelButton.removeAll();
-        for (int i=0; i<grenzeX; i++) {
-            for (int j=0; j<grenzeY; j++) {
-                figur = listTmp.get(index);
-                spielfeld.figurInit(figur,i,j);
-            }
-        }
+
+        spielfeld.figurInit(figurenSatzKI.get(0), figurenSatzKI.get(0).getPosition().getX(), figurenSatzKI.get(0).getPosition().getY());
+
         spielfeld.panelAktualisieren();
     }
 
@@ -67,14 +60,47 @@ public class SchwereKI extends KI {
 
     private Position[] setzeBomben(){
         int z=(int)(Math.random()*4);
-        Position fahne= listTmp.get(0).getPosition(); // Position der Fahne
+        Position fahne=figurenSatzKI.get(0).getPosition(); // Position der Fahne
         Position bomben [] = new Position[6];
-        switch(z){
-            case 0:
-                if(fahne.getX()==0 && fahne.getY()==0) {
-                    bomben[0].setX(fahne.getX() + 1);
-                    bomben[0].setY(fahne.getY() + 1);
-                }
+        int fahneX=fahne.getX();
+        int fahneY=fahne.getY();
+        // Fahne steht in einer der Ecken
+        if(fahneX==0 && (fahneY==0 || fahneY==9)) {
+            switch (z) {
+                // Fahne wird direkt eingekesselt und Tarnung aufgebaut
+                case 0:
+                    bomben[0] = new Position(0, 1); // Fahne einkesslen
+                    bomben[1] = new Position(1, 0);
+                    bomben[2] = new Position(0, 8); // andere Ecke zur Tarnung einkesseln
+                    bomben[3] = new Position(1, 9); // restliche Bomben wahllos setzen
+                    break;
+                // Fahne wird direkt eingekesselt, aber keine Tarnung
+                case 1:
+                    if (fahneY == 0) {
+                        bomben[0] = new Position(0, 1); // Fahne einkesslen
+                        bomben[1] = new Position(1, 0);
+                    } else {
+                        bomben[0] = new Position(0, 8); // andere Ecke zur Tarnung einkesseln
+                        bomben[1] = new Position(1, 9);
+                    }
+                    break;
+                // Fahne wird von Unteroffizieren und aehnlichen geschuetzt davor Bomben
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        // Fahne steht irgendwo in den ersten beiden Reihen
+        } else{
+
+        }
+        // bisher nicht gesetzte Bomben muessen (zufaellig) gesetzt werden
+        for(int i=0; i<bomben.length; i++){
+            if(bomben[i]==null){
+                int x= (int)(Math.random()*3);
+                int y=(int)(Math.random()*10);
+                // Test ob Feld belegt
+            }
         }
         return null;
     }
